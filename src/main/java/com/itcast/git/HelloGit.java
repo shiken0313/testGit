385ae93322dd929298b525e078516e8d674895d4
sro800313 <sro800313@gmail.com>
2021-02-03 21:00:50 +0800

    這樣

--- src/main/java/com/itcast/git/HelloGit.java
+++ src/main/java/com/itcast/git/HelloGit.java
@@ -4,21 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-
-		System.out.println("��s");
-		System.out.println("�Ыؤ���");
-
-		System.out.println("�D�u�K�[");
-
-		System.out.println("����K�[");
-		System.out.println("���椧�e��s");
-
-		System.out.println("§ó·s");
-		System.out.println("³Ð«Ø¤À¤ä");
-
-		System.out.println("¥D½u²K¥[");
-
-		System.out.println("¤À¤ä²K¥[");
+		System.out.println("是這樣嗎?");
 		System.out.println("在線添加");
 
 	}
