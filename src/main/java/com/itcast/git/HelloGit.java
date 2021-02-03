0b431defe73a2e23a87f1bbcb4c7d90a7a0864e3
shiken0313 <63995567+shiken0313@users.noreply.github.com>
2021-02-03 20:58:14 +0800

    Update HelloGit.java
    更新

--- src/main/java/com/itcast/git/HelloGit.java
+++ src/main/java/com/itcast/git/HelloGit.java
@@ -4,12 +4,6 @@ public class HelloGit {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("§ó·s");
-		System.out.println("³Ð«Ø¤À¤ä");
-
-		System.out.println("¥D½u²K¥[");
-
-		System.out.println("¤À¤ä²K¥[");
 		System.out.println("在線添加");
 
 	}
